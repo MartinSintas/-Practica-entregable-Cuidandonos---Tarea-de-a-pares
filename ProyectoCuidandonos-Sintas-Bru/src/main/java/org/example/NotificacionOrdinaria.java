a61093218ef37e01939978f90a54d642e2a3a721
Bru Nicolas <82009198+Bru-Nicolas-FRBA@users.noreply.github.com>
2024-04-15 12:52:58 -0300

    Delete ProyectoCuidandonos-Sintas-Bru/src/main/java/org/example directory
    borro otro

--- ProyectoCuidandonos-Sintas-Bru/src/main/java/org/example/NotificacionOrdinaria.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-public class NotificacionOrdinaria {
-    public void notificar(Configuracion configuracion) {
-        //printf con la configuracion
-    }
-}
--- ProyectoCuidandonos-Sintas-Bru/src/main/java/org/example/Sexo.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-
-public enum Sexo {
-    HOMBRE,
-    MUJER
-}
