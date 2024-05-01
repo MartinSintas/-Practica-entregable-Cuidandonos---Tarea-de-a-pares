1a6e6e70cddb13b109965d61fd5ce91c21619d56
Bru Nicolas <82009198+Bru-Nicolas-FRBA@users.noreply.github.com>
2024-05-01 13:26:42 -0300

    Add files via upload

---
+++ ProyectoCuidandonos-Sintas-Bru/src/main/java/Enums/Configuracion.java
@@ -0,0 +1,8 @@
+package Enums;
+
+public enum Configuracion {
+    MENSAJE,
+    POLICIA,
+    LLAMADA,
+    FALSA_ALARMA
+}
---
+++ ProyectoCuidandonos-Sintas-Bru/src/main/java/Notificacion/NotificacionOrdinaria.java
@@ -0,0 +1,11 @@
+package Notificacion;
+
+import Viaje.Viaje;
+
+public class NotificacionOrdinaria {
+    public void notificar(Viaje configuracion) {
+
+        //printf con la configuracion
+
+    }
+}
